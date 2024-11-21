01d47301e92a6a5cd20640449871363a6a8e3a68
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-11-21 10:29:12 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,6 @@
 echo "Sai is learning Python"
 echo "Sai is learning DevOps"
+echo "Praveen is also learning DevOps"
+echo "Praveen is also learning Python"
+echo "Suresh is learning Java"
+echo "Suresh is learning Ruby"
