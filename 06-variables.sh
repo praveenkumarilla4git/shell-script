579430e649b2f3694dcc477594f316a09ae5ff4f
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-11-25 11:11:48 +0530

    args

---
+++ 06-variables.sh
@@ -0,0 +1,10 @@
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1 is learning Python"
+echo "$PERSON1 is learning DevOps"
+echo "$PERSON2 is also learning DevOps"
+echo "$PERSON2 is also learning Python"
+echo "$PERSON3 is learning Java"
+echo "$PERSON3 is learning Ruby"
