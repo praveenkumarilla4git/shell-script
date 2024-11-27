8939eea5c78d6baea1e9f8fa5b6761dae8c37418
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-11-27 10:55:12 +0530

    uploading new read ech hide variables.sh

--- 07-variables.sh
+++ 07-variables.sh
@@ -7,6 +7,9 @@ echo "Please enter your username"
 
 read USERNAME # attached to username variable
 
+echo "Please enter your passsword"
+read -s PASSWORD # attached to password variable
+
 
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
