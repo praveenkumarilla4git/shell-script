059435dc26a549892b8f720007156822ff7885cf
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-11-27 11:31:43 +0530

    arrays

--- 11-arrays.sh
+++ 11-arrays.sh
@@ -3,3 +3,5 @@ cars=("dezire" "punch" "XUV700")
 echo "First value in the given array is: ${cars[0]}"
 echo "Second value in the given array is: ${cars[1]}"
 echo "Third value in the given array is: ${cars[2]}"
+
+echo "All cars: ${cars[@]}"
