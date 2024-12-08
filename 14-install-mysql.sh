59899a21fb1738881623ef66e8d076dfa54eea83
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-12-08 10:52:42 +0530

    testing

---
+++ 14-install-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you want to exit
+else
+    echo "you are root user"
+fi
+
+yum install python -y
