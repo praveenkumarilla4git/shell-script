cf5481bed8d49211b018adc45464f5ba0e943186
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-12-08 11:34:04 +0530

    testing

--- 15-install-git.sh
+++ 15-install-git.sh
@@ -11,4 +11,21 @@ else
 fi
 
 yum install git -y
+
+if [ "$?" -ne 0 ]
+then
+    echo "git installation is failed"
+    exit 1 # you want to exit
+else
+    echo "git installation is success"
+fi
+
 yum install python -y
+
+if [ "$?" -ne 0 ]
+then
+    echo "python installation is failed"
+    exit 1 # you want to exit
+else
+    echo "python installation is success"
+fi
