668ee0b9ebbb0db2c00422d4ca3f525e5091e6e4
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-12-10 11:00:12 +0530

    updated mysql install

--- 14-install-mysql.sh
+++ 14-install-mysql.sh
@@ -12,3 +12,7 @@ fi
 
 yum install git -y
 yum install python -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
+sudo yum localinstall -y mysql57-community-release-el7-8.noarch.rpm
+sudo yum install -y mysql-community-server
