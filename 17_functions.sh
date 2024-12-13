63a75e2357f50b222f80a60abd13df228ebb70a5
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-12-13 10:48:47 +0530

    shellfunction

---
+++ 17_functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ "$?" -ne 0 ]
+    then
+        echo "python installation is failed"
+        exit 1 # you want to exit
+    else
+        echo "python installation is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: please run the script with root access"
+    exit 1
+else
+    echo "You are root user"
+
+yum install python -y
+
+VALIDATE
