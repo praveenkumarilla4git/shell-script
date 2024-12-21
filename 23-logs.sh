c1d327a278504c3e35f0a083c0ad44d5ea8afcc1
praveenkumarila4git <praveenkumarilla@gmail.com>
2024-12-21 09:56:15 +0530

    echocolorpallet

--- 23-logs.sh
+++ 23-logs.sh
@@ -3,7 +3,9 @@
 ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
+LOG_DIR="./logs"
+mkdir -p "$LOG_DIR"
+LOGFILE="./logs/$0-$TIMESTAMP.log"
 
 echo "script name: $0"
 
@@ -13,14 +15,14 @@ VALIDATE(){
         echo "ERROR:: $2 .. failed"
         exit 1
     else
-        echo "$2 successful"
+        echo -e "$2 \e[32m successful \e[0m"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exist 1
+    exit 1
 else
     echo "You are the root user"
 fi
